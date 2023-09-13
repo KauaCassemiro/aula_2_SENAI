5d23c89ffbbe3d6947bfa054ef10c454173806d6
KauaCassemiro <111397958+KauaCassemiro@users.noreply.github.com>
2023-09-13 16:49:48 -0300

    Add files via upload

---
+++ Aula 2.py
@@ -0,0 +1,18 @@
+#Atividade 1
+num1 = int (input("Digite um numero"))
+num2 = int (input("Digite outro numero"))
+print(num1 + num2)
+
+#Atividade 2
+nome = "Kauã"
+print(nome)
+
+#Atividade 3
+text = "Olá "
+nome = "Kauã"
+print(text + nome)
+
+#Atividade 4
+num1 = int (input("Digite um numero"))
+num2 = int (input("Digite outro numero"))
+print(num1 * num2)
