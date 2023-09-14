d4dd2611b225b4ecd018447e3ff067913c0564f4
KauaCassemiro <111397958+KauaCassemiro@users.noreply.github.com>
2023-09-14 15:56:48 -0300

    Add files via upload

---
+++ Aula3.py
@@ -0,0 +1,35 @@
+#at 1
+n1 = 2
+print(n1 * n1)
+
+#at 2 
+nome = "Kauã "
+nome2 = "Cassemiro"
+print(nome + nome2)
+
+#at 3
+num1 = int(input("Digite um numero"))
+num2 = int(input("Digite outro numero"))
+print(num1,num2)
+
+#at 4
+text = "Python"
+num1 = int(input("Digite um numero"))
+print(text , num1)
+
+#at 5
+palavra = "Python" 
+frase = "Qual linguagem vc ta usando? "
+print(frase , palavra)
+
+#at 6
+hh = "14:"
+mm = "27:"
+ss = "50"
+print(hh , mm , ss)
+
+#at 6a
+num1 = int(input("Digite a hora"))
+num2 = int(input("Digite o minuto"))
+num3 = int(input("Digite os segundos"))
+print(num1, ":", num2, ":",num3)
